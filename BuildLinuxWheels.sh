27fc4255576ecd0e4e1df2fd52d543dfde3482e9
Darren Thompson <darren.thompson@csiro.au>
2018-10-19 20:07:38 +1100

    mods

--- BuildLinuxWheels.sh
+++ BuildLinuxWheels.sh
@@ -36,4 +36,30 @@ if [ $ITK_PACKAGE_VERSION == v4.13.1.post1 ]; then
 fi
 
 # Finally build Linux wheels
-./ITKPythonPackage/scripts/dockcross-manylinux-build-module-wheels.sh
+#./ITKPythonPackage/scripts/dockcross-manylinux-build-module-wheels.sh
+
+# Run this script to build the Python wheel packages for Linux for an ITK
+# external module.
+#
+# Versions can be restricted by passing them in as arguments to the script
+# For example,
+#
+#   scripts/dockcross-manylinux-build-module-wheels.sh cp27mu cp35
+
+# Pull dockcross manylinux images
+docker pull dockcross/manylinux-x64
+
+# Generate dockcross scripts
+docker run dockcross/manylinux-x64 > /tmp/dockcross-manylinux-x64
+chmod u+x /tmp/dockcross-manylinux-x64
+
+script_dir=$(cd $(dirname $0) || exit 1; pwd)
+
+# Build wheels
+mkdir -p dist
+DOCKER_ARGS="-v $(pwd)/dist:/work/dist/ -v $script_dir/..:/ITKPythonPackage -v $(pwd)/tools:/tools -v /usr/local/cuda:/usr/local/cuda"
+echo "DOCKER_ARGS = " $DOCKER_ARGS
+/tmp/dockcross-manylinux-x64 \
+  -a "$DOCKER_ARGS" \
+  "/ITKPythonPackage/scripts/internal/manylinux-build-module-wheels.sh" "$@"
+
